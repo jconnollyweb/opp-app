73c7dda2485be959f352e15d2bf00008a6df5cae
jconnollyweb <johnc1983@hotmail.co.uk>
2024-03-07 15:47:33 +0000

    styles

--- opps-app/src/pages/RoleDetails.js
+++ opps-app/src/pages/RoleDetails.js
@@ -13,20 +13,20 @@ const RoleDetails = () => {
   }
 
   return (
-    <div className='main-container'> 
-    <div className='details-container'>
-      <div className='title-container'>
+    <div className={styles.mainContainer}> 
+    <div className={styles.detailsContainer}>
+      <div className={styles.titleContainer}>
        <h2>{role.account}</h2>
       </div>
-      <div className='both-sides'>
-      <div className='left-side'>
+      <div className={styles.bothSides}>
+      <div className={styles.leftSide}>
           <p>Start Date: {role.startDate}</p>
           <p>End Date: {role.endDate}</p>
           <p>Revenue: {role.revenue}</p>
           <p>Owner: {role.owner}</p>
           <p>Forecast: {role.forecast}</p>
       </div>
-      <div className='right-side'>
+      <div className={styles.rightSide}>
           <p>Role: {role.role}</p>
           <p>Location: {role.location}</p>
           <p>Grade: {role.grade} </p>
@@ -36,7 +36,7 @@ const RoleDetails = () => {
       
      
      
-      <Link className='link-btn' to={'/opps-app'}>Back</Link>
+      <Link className={styles.linkBtn} to={'/opps-app'}>Back</Link>
     </div>
     </div>
   );
